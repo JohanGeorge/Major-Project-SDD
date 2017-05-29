b49eeea3b03784d5f997fca0a8239efffa72f8f2
lucasmasina <pitch3r.luc@gmail.com>
2017-05-29 11:29:27 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+JACK WAGNER SUX
+</head>
+<body>
+<h1>This is Lucas' Website!</h1>
+
+<form action="process.php" method="post">
+	Enter your name:
+	<input name="name" type="text">
+	
+	Pick a number between 1 and 10!
+	<input name="number" type="number">
+	
+	Enter the annual investment
+	<input name="s" type="number">
+	
+	Enter the rate of interest (the percent divided by 100)
+	<input name="r" type="float">
+	
+	Enter the number of years for its creation
+	<input name="n" type="number">
+	
+	<input type="submit">
+</form>
+
+<?php
+	$login = False;
+	if ($login == True) {
+		echo "You're in!";
+	}	else{
+			echo "Please Try again!";
+		}
+	?>
+</body>
+</html>
