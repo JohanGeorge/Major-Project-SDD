d1514a9dfb269ed2c61b48e9e9f0260070b88d71
lucasmasina <pitch3r.luc@gmail.com>
2017-06-02 18:48:27 +1000

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,21 @@ School Newsletter
 </head>
 <body>
 
+<form action="process.php" method="post">
+	Enter your Username:
+	<input name="Username" type="text" size= "40" placeholder="Username"></br></br>
+
+	Enter your Password:
+	<input name="Password" type="text" size= "40" placeholder="Password"></br></br>
+	
+	Confirm Password:
+	<input name="CPassword" type="text" size= "40" placeholder="Confirm Password">
+
 		
-		
+	<input type="submit" value="Register &rarr;">
+</form>
+	
+<marquee><blink>THAT hkhkjhkhljhl</blink></marquee>
 
 </body>
 </html>
