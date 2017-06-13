88a8c324960642dec6c540ca6b46164223c616b1
lucasmasina <pitch3r.luc@gmail.com>
2017-06-13 18:20:44 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <?php
-	session_start();
+	include 'header.php';
 ?>
 
-<!Doctype html>
-<html>
-<head>
-<title> 
-School Newsletter
-</title>
 
-
-</head>
-<body>
-
-
-<form action="login.php" method="post">
-	Log In </br></br>
-	<input name="uid" type="text" size= "40" placeholder="Username"></br></br>
-
-	<input name="pwd" type="password" size= "40" placeholder="Password">
-	<button type="submit">LOG IN &rarr;</button>
-</form></br>
 
 <?php
 	if(isset($_SESSION['id'])){
@@ -29,21 +11,6 @@ School Newsletter
 		echo "you are not logged in";
 	}
 ?>
-</br></br>
-<form action="process.php" method="post">
-	Sign Up</br></br>
-	<input name="first" type="text" size= "40" placeholder="First Name"></br></br>
-
-	<input name="last" type="text" size= "40" placeholder="Last Name"></br></br>
-	
-	<input name="uid" type="text" size= "40" placeholder="Username"></br></br>
-
-	<input name="pwd" type="password" size= "40" placeholder="Password">
-	<button type="submit">SIGN UP &rarr;</button>
-</form> </br></br></br>
-<form action="logout.php">
-	<button>LOG OUT</button>
-</form>
 
 </body>
 </html>
