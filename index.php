6809951c7f2e987fa089ab6ea6561bf5ac2c42c0
lucasmasina <pitch3r.luc@gmail.com>
2017-06-13 21:30:09 +1000

    Create index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 	include 'header.php';
 ?>
 
-
-
 <?php
 	if(isset($_SESSION['id'])){
 		echo $_SESSION['id'];
@@ -11,6 +9,11 @@
 		echo "you are not logged in";
 	}
 ?>
+<form action="upload.php" method="POST" enctype="multipart/form-data"> 
+	<input type="file" name="file" >
+	<button type="submit" name="submit">UPLOAD</button>
+
+</form>
 
 </body>
 </html>
