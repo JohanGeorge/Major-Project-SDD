e25c4343791cce1b02ac1ee1e8fdd70389767557
lucasmasina <pitch3r.luc@gmail.com>
2017-06-15 20:11:26 +1000

    Create index.php

--- index.php
+++ index.php
@@ -5,15 +5,22 @@
 <?php
 	if(isset($_SESSION['id'])){
 		echo $_SESSION['id'];
+		echo '<form action="upload.php" method="POST" enctype="multipart/form-data"> 
+				<input type="file" name="file" >
+				<button type="submit" name="submit">UPLOAD</button>
+				</form>';
 	}else{
 		echo "you are not logged in";
 	}
 ?>
-<form action="upload.php" method="POST" enctype="multipart/form-data"> 
-	<input type="file" name="file" >
-	<button type="submit" name="submit">UPLOAD</button>
 
-</form>
-
+<?php
+	if(isset($_SESSION['id'])){
+		if(isset($_SESSION['filedestination'])){
+			echo "<img src='" . $_SESSION['filedestination'] . "' style='width:50px;height:50px;' alt='error'>" ;
+		}else{echo "No image selected";
+		}
+	}
+?>
 </body>
 </html>
