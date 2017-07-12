9f083c35b669f153b12499c276633a4c40ad5aaa
lucasmasina <pitch3r.luc@gmail.com>
2017-07-12 21:20:51 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 	include 'header.php';
-?>
+	
+	$conn = mysqli_connect("localhost", "root", "", "images");
 
+	if (!$conn) {
+		die("connection failed: ".mysqli_connect_error());
+	}
+?>
 <?php
 	if(isset($_SESSION['id'])){
 		echo $_SESSION['id'];
@@ -13,14 +18,18 @@
 		echo "you are not logged in";
 	}
 ?>
-
 <?php
-	if(isset($_SESSION['id'])){
-		if(isset($_SESSION['filedestination'])){
-			echo "<img src='" . $_SESSION['filedestination'] . "' style='width:50px;height:50px;' alt='error'>" ;
-		}else{echo "No image selected";
+	$sql = "SELECT filedestination FROM filedestinations";
+	$query = mysqli_query($conn, $sql);
+	if(mysqli_query($conn, $sql)){
+		//output data from every row
+		while($row = mysqli_fetch_assoc($query)){
+			echo "<img src='" . $row['filedestination'] . "' style='width:50px;height:50px;' alt='error'>" ;
 		}
-	}
+	}else{
+		echo "Error displaying image";
+		}
 ?>
+
 </body>
 </html>
