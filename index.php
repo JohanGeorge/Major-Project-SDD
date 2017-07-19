92fd9572873a429a02c71f1a9c68787b7855aa91
lucasmasina <pitch3r.luc@gmail.com>
2017-07-19 15:54:28 +1000

    Update index.php

--- index.php
+++ index.php
@@ -9,22 +9,21 @@
 
 	if(isset($_SESSION['id'])){
 		//echo $_SESSION['id'];
-		echo '<form action="upload.php" method="POST" enctype="multipart/form-data"> 
+		echo '<div class="upload">	
+				<form action="upload.php" method="POST" enctype="multipart/form-data"> 
 				<input type="file" name="file" >
 				<input type="text" name="description" >
 				<button type="submit" name="submit">UPLOAD</button>
-				</form>';
-	}else{
-		echo "you are not logged in";
+				</form></div>';
 	}
 
-	$sql = "SELECT filedestination,description FROM filedestinations";
+	$sql = "SELECT filedestination,description FROM filedestinations ORDER BY id DESC";
 	$query = mysqli_query($conn, $sql);
 	if(mysqli_query($conn, $sql)){
 		//output data from every row
 		while($row = mysqli_fetch_assoc($query)){
 			$image = "<img src='" . $row['filedestination'] . "' style='width:100px;height:100px;' alt='error'>" ;
-			$desc = $row['description'];
+			$desc = "<div class='desc'>" . $row['description'] . "</div>";
 			echo "<div class='post'>" . $image . $desc . "</div>";
 		}
 	}else{
