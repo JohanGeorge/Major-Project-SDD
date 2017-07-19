d8af8c803be01ffc3217b499e5e446d9ab0c7c62
lucasmasina <pitch3r.luc@gmail.com>
2017-07-19 11:36:56 +1000

    Update index.php

--- index.php
+++ index.php
@@ -6,30 +6,32 @@
 	if (!$conn) {
 		die("connection failed: ".mysqli_connect_error());
 	}
-?>
-<?php
+
 	if(isset($_SESSION['id'])){
-		echo $_SESSION['id'];
+		//echo $_SESSION['id'];
 		echo '<form action="upload.php" method="POST" enctype="multipart/form-data"> 
 				<input type="file" name="file" >
+				<input type="text" name="description" >
 				<button type="submit" name="submit">UPLOAD</button>
 				</form>';
 	}else{
 		echo "you are not logged in";
 	}
-?>
-<?php
-	$sql = "SELECT filedestination FROM filedestinations";
+
+	$sql = "SELECT filedestination,description FROM filedestinations";
 	$query = mysqli_query($conn, $sql);
 	if(mysqli_query($conn, $sql)){
 		//output data from every row
 		while($row = mysqli_fetch_assoc($query)){
-			echo "<img src='" . $row['filedestination'] . "' style='width:50px;height:50px;' alt='error'>" ;
+			$image = "<img src='" . $row['filedestination'] . "' style='width:100px;height:100px;' alt='error'>" ;
+			$desc = $row['description'];
+			echo "<div class='post'>" . $image . $desc . "</div>";
 		}
 	}else{
 		echo "Error displaying image";
 		}
 ?>
+	<div class='footer'>Email: newsletter@gmail.com</div>
 
 </body>
 </html>
