04893e45309c54332056e4c51024981e3fada490
James Groom <yoshiisawsm@gmail.com>
2017-07-31 17:21:44 +1000

    Fix indent, spacing, etc.

--- header.php
+++ header.php
@@ -1,39 +1,31 @@
-<?php
-	session_start();
-?>
-
-<!Doctype html>
+<?php session_start(); ?>
+<!DOCTYPE html>
 <html>
-<head>
-<title> 
-School Newsletter
-</title>
-
-<link rel="stylesheet" type="text/css" href = "style.css">
-
-</head>
-<body>
-
-<header>
-	<nav>
-		<ul>
-			<li><div class="icon"><img src="newicon.png" style="width:50px;height:50px;"></div></li>
-			<li><a href="index.php">HOME</a></li>
-				<?php
-					if(isset($_SESSION['id'])){
-						echo "<form action='include/logout.inc.php'>
-							<button>LOG OUT</button>
-						</form>";
-					}else{
-						echo "<form action='include/login.inc.php' method='post'>
-						<input name='uid' type='text' size= '40' placeholder='Username'>
-						<input name='pwd' type='password' size= '40' placeholder='Password'>
-						<button type='submit'>LOG IN &rarr;</button>
-						</form>";
-					}
-				?>
-			<li><a href="signup.php">SIGNUP</a></li>
-		</ul>
-	</nav>
-
-</header>
+	<head>
+		<title>School Newsletter</title>
+		<link rel="stylesheet" type="text/css" href = "style.css">
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li><div class="icon"><img src="newicon.png" style="width: 50px; height: 50px;"></div></li>
+					<li><a href="index.php">HOME</a></li>
+					<li><?php
+						if (isset($_SESSION['id'])) {
+							echo "<form action='include/logout.inc.php'>\n" .
+									"<button type='submit'>LOG OUT</button>\n" .
+								"</form>";
+						} else {
+							echo "<form action='include/login.inc.php' method='post'>\n" .
+									"<input name='uid' type='text' size='40' placeholder='Username'>\n" .
+									"<input name='pwd' type='password' size='40' placeholder='Password'>\n" .
+									"<button type='submit'>LOG IN &rarr;</button>\n" .
+								"</form>";
+						}
+					?></li>
+					<li><a href="signup.php">SIGNUP</a></li>
+					<li><a href="https://yoshirulz.github.io/SDD-Project">A BETTER WEBSITE</a></li>
+				</ul>
+			</nav>
+		</header>
